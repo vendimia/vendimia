073488f163b0c6cf172c237c6cbd4bb6a2379d23
Oliver Etchebarne <oliver@icaserver.com>
2017-06-10 11:05:38 -0500

    A collection of Entities

---
+++ base/libs/ORM/EntitySet.php
@@ -0,0 +1,159 @@
+<?php
+namespace Vendimia\ORM;
+
+use Vendimia\AsArrayInterface;
+
+/**
+ * Collection of entities
+ */
+class EntitySet implements \Iterator
+{
+    use QueryManager;
+
+    /** This FQCN **/
+    private $base_class;
+
+    /** Database table, copied from static property */
+    private $db_table;
+
+    /** Database connector, copied from static property */
+    private $db_connector;
+
+    /** Cursor returned by the database connector */
+    private $cursor;
+
+    /** Last retrieved entity */
+    private $last_entity;
+
+    /** Iterator index */
+    private $iterator_index;
+
+    /** Is this set already retrieved? */
+    private $set_retrieved = false;
+
+    /**
+     * Dummy function. Is not neede here, but trait QueryManager needs it
+     */
+    private static function configureStatic()
+    {
+    }
+
+    /**
+     * Sets up a new Query EntitySet 
+     */
+    public function __construct($base_class, $db_table, $db_connector)
+    {
+        $this->base_class = $base_class;
+        $this->db_table = $db_table;
+        $this->db_connector = $db_connector;
+        $this->whereBuilder = new Where;
+
+        $this->whereBuilder->setConnector($db_connector);
+        $this->whereBuilder->setPrimaryKeyField($base_class::getPrimaryKeyField());
+    }
+
+    /**
+     * Executes the query
+     */
+    public function retrieveSet($force = false)
+    {
+        if ($this->set_retrieved && !$force) {
+            return false;
+        }
+        $this->cursor = $this->executeQuery();
+        $this->set_retrieved = true;
+    }
+
+    /**
+     * Returns the next record in this set
+     */
+    public function fetch() 
+    {
+        $this->retrieveSet();
+
+        $data = $this->db_connector->fetchOne($this->cursor);
+        if (!$data) {
+            $this->is_empty = true;
+            return null;
+        }
+
+        $class = $this->base_class;
+
+        return new $class($data, true); // $not_new = true
+    }
+
+    /**
+     * Returns the registry count
+     */
+    public function count()
+    {
+        return intval($this->executeSQLFunction('count', '*'));
+    }
+
+    /**
+     * Executes a single SQL function on a field
+     */
+    private function executeSQLFunction($function, $field)
+    {
+        // Ejecutamos la función en un objeto distinto
+        $target = clone $this;
+        
+        $target->query['fields'] = ["{$function}({$field})" => "__vendimia_function_result"];
+        $c = $target->executeQuery();
+
+        $data = $this->db_connector->fetchOne($c);
+
+        return $data['__vendimia_function_result'];
+    }
+
+
+    /**
+     * {@inherit}
+     */
+    public function current()
+    {
+        return $this->last_entity;
+    }
+
+    /**
+     * {@inherit}
+     */
+    public function key()
+    {
+        return $this->iterator_index;
+    }
+
+    /**
+     * {@inherit}
+     */
+    public function next()
+    {
+
+    }
+
+    /**
+     * {@inherit}
+     */
+    public function rewind()
+    {
+        $this->retrieveSet(true);
+        $this->iterator_index = 0;        
+    }
+
+    /**
+     * {@inherit}
+     */
+    public function valid()
+    {
+        $entity = $this->fetch();
+
+        if (is_null($entity)) {
+            $this->is_empty = true;
+            return false;
+        }
+
+        $this->last_entity = $entity;
+        $this->iterator_index++;
+        return true;
+    }
+}
