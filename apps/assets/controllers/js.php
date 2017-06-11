bad65e26ee469364696d6b641723f1d50ef96f45
Oliver Etchebarne <oliver@icaserver.com>
2017-06-11 11:42:11 -0500

    Remove old code, added a ; after each .js script

--- apps/assets/controllers/js.php
+++ apps/assets/controllers/js.php
@@ -24,61 +24,8 @@ foreach ($files as $file) {
     }
    
     $file = $fp->get();
-    $js .= file_get_contents($file) . "\n";
+    $js .= file_get_contents($file) . ";\n";
 }
 
 (new Http\Response($js, 'application/javascript'))
     ->send();
-
-
-// Si no hay argumentos, 500!
-if (!isset (Vendimia::$args[0]) || trim(Vendimia::$args[0]) == "") {
-    Http\Response::serverError("You must specify at least one Javascript asset filename.");
-}
-$path = join_paths(Vendimia::$ARGS->get() );
-
-// Hay una app por defecto?
-$colon = strpos ($path, '::' );
-if ( $colon !== false ) {
-    // Cambiamos el nombre de la app. No creo que haya problemas...
-    Vendimia::$APPLICATION = strtr(substr ( $path, 0, $colon ), '.', '/');
-    $path = ( substr ( $path, $colon + 2 ));
-}
-
-
-$names = explode (',', $path );
-
-// Verificamos los etags de todos los ficheros que pide. Si ninguno
-// ha variado, entonces 304.
-$files = [];
-$modified = true;
-foreach ( $names as $name ){
-
-    // Si no tienen extensión .css, la añadimos
-    /*if ( substr ( $name, -3 ) != ".js" )
-        $name .= '.js';*/
-
-    $fp = new Vendimia\Path\FileSearch($name, 'assets/js', 'js');
-
-    // Si no existe, 404!
-    if ($fp->notFound()) {
-        http\file_not_found ( "Javascript asset file '$name' not found", [
-            'Search paths' => $fp->searched_paths,
-        ]);
-    }
-    
-    $file = $fp->get();
-    $S = stat ($file);
-    
-    $files[] = $file;
-}
-
-if ( $modified ) {
-    // Enviamos todos los ficheros
-    header ( 'Content-Type: text/javascript');
-    foreach ( $files as $f ) {
-        echo file_get_contents( $f ) . "\n";
-    }
-}
-
-exit;
