90aedb738873c1ead7ab077b6bd05a1cc455beae
Oliver Etchebarne <yo@drmad.org>
2018-09-23 23:38:05 -0500

    Smally caps typo corrected

--- base/libs/ORM/Entity.php
+++ base/libs/ORM/Entity.php
@@ -424,7 +424,7 @@ abstract class Entity implements AsArrayInterface, ValueInterface
     /**
      * AsArrayInterface implementation
      */
-    public function AsArray($fields = null)
+    public function asArray($fields = null)
     {
         $this->retrieveRecord();
         $result = [];
