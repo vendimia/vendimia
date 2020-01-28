05b411b0cd44e36fd30786e183db45094f63c1ed
Oliver Etchebarne <yo@drmad.org>
2020-01-27 21:33:37 -0500

    Small changes in controller and view templates

--- bin/FileTemplates/default-controller.php
+++ bin/FileTemplates/default-controller.php
@@ -3,11 +3,16 @@ namespace <?=$app_name?>\Controller;
 
 use Vendimia as V;
 use Vendimia\Http\Response;
+use Vendimia\Routing\Rule;
+use Vendimia\Controller\WebController;
+use <?=$app_name?>\Model;
+use <?=$app_name?>\Entity;
+use <?=$app_name?>\Form;
 
 /**
  * Default controller for '<?=$app_name?>' app.
  */
-class DefaultController extends V\ControllerBase
+class DefaultController extends WebController
 {
     public function default()
     {
--- bin/FileTemplates/default-view.php
+++ bin/FileTemplates/default-view.php
@@ -5,5 +5,5 @@ namespace <?=$app_name?>; use Vendimia as V, Vendimia\Html, Vendimia\Url;?>
 <h1>Hi! I'm the <em><?=$controller_name?></em> view from <em><?=$app_name?></em> application!</h1>
 
 <p>Now, edit the view file <strong>apps/<?=$app_name?>/views/<?=$controller_name?>.php</strong> for
-changing this text, or the method <tt><?=$controller_name?></tt> in the <strong>apps/<?=$app_name?>/Controller.php</strong> file
+changing this text, or the method <tt><?=$controller_name?></tt> in the <strong>apps/<?=$app_name?>/Controller/DefaultController.php</strong> file
 for changing this controller behavior.</p>
