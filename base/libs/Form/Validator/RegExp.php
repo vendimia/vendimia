3cf0f9b630fb8580aecdf4082dab293c348c0226
Oliver Etchebarne <1307948+drmad@users.noreply.github.com>
2020-07-13 10:56:16 -0500

    Change message code to 'no_match'

--- base/libs/Form/Validator/RegExp.php
+++ base/libs/Form/Validator/RegExp.php
@@ -2,17 +2,18 @@
 namespace Vendimia\Form\Validator;
 
 /**
- * Validates a value against a regular expression. 
+ * Validates a value against a regular expression.
  *
- * The regexp is surrounded by the '%' character, so it must be escaped if 
+ * The regexp is surrounded by the '%' character, so it must be escaped if
  * used inside.
  */
 class RegExp extends ValidatorAbstract
 {
     protected $args = [
         'regexp' => null,
+        'case_insensitive' => false,
         'messages' => [
-            'fail' => '%control% contains invalid characters.',
+            'no_match' => '%control% contains invalid characters.',
         ]
     ];
     public function validate()
@@ -22,7 +23,7 @@ class RegExp extends ValidatorAbstract
             $options .= 'i';
         }
         if (preg_match("%{$this->args['regexp']}%{$options}", $this->control->value) === 0) {
-            $this->addMessage('fail');
+            $this->addMessage('no_match');
             return false;
         }
         return true;
